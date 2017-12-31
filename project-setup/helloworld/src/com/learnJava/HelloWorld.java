050faa50887b2a7655bd45eb20818aba345fca92
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-12-30 18:20:53 -0600

    added the intial project

---
+++ project-setup/helloworld/src/com/learnJava/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.learnJava;
+
+/**
+ * Created by z001qgd on 12/30/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Hello World");
+    }
+}
